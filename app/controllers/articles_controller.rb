8977f71dc32d33f06a2140cfc2d4251c722f681d
psidae <akson.rill@gmail.com>
2018-12-17 19:30:18 +0100

    add card

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
 class ArticlesController < ApplicationController
-
+    def index; end
+    def show; end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,20 @@ class SessionsController < ApplicationController
     def new; end       
     
     def create
-        
+        user = User.find_by(email: params[:logins][:email].downcase)
+        if user && user.authenticate(params[:logins][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "You have successfully logged in"
+            redirect_to user
+        else
+            flash.now[:notice] = "Something wrong with your login information"
+            render 'new'
+        end
     end
 
     def destroy
-        
+        session[:student_id] = nil
+        flash[:notice] = "You have successfully logged out"
+        redirect_to root_path
     end
 end
